754c2e107406be196344eb07b793360d00d79c6a
Bastian Ike <bastian.ike@aoe.com>
2021-07-19 09:11:42 +0200

    extend mocks

--- mock.go
+++ mock.go
@@ -2,14 +2,17 @@ package grpc
 
 import (
 	"context"
+	"encoding/json"
 
 	"flamingo.me/flamingo/v3/core/auth"
 	"flamingo.me/flamingo/v3/framework/config"
+	"golang.org/x/oauth2"
 )
 
 type mockCallIdentifier struct {
 	identifier string
 	subject    string
+	claims     []byte
 }
 
 var _ CallIdentifier = new(mockCallIdentifier)
@@ -18,6 +21,7 @@ func mockFactory(cfg config.Map) (CallIdentifier, error) {
 	var config struct {
 		Identifier string
 		Subject    string
+		Claims     string
 	}
 
 	if err := cfg.MapInto(&config); err != nil {
@@ -27,6 +31,7 @@ func mockFactory(cfg config.Map) (CallIdentifier, error) {
 	return &mockCallIdentifier{
 		identifier: config.Identifier,
 		subject:    config.Subject,
+		claims:     []byte(config.Claims),
 	}, nil
 }
 
@@ -37,6 +42,7 @@ func (identifier *mockCallIdentifier) Identifier() string {
 type mockIdentity struct {
 	identifier string
 	subject    string
+	claims     []byte
 }
 
 func (identity *mockIdentity) Broker() string {
@@ -47,9 +53,18 @@ func (identity *mockIdentity) Subject() string {
 	return identity.subject
 }
 
+func (identity *mockIdentity) AccessTokenClaims(into interface{}) error {
+	return json.Unmarshal(identity.claims, into)
+}
+
+func (identity *mockIdentity) TokenSource() oauth2.TokenSource {
+	return nil
+}
+
 func (identifier *mockCallIdentifier) Identify(ctx context.Context) (auth.Identity, error) {
 	return &mockIdentity{
 		identifier: identifier.identifier,
 		subject:    identifier.subject,
+		claims:     identifier.claims,
 	}, nil
 }
